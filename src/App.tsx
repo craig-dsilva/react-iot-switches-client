7f690eb163bb9864ba51df2da5217c665ec26af7
craig-dsilva <craig.s.dsilva@gmail.com>
2022-08-24 20:59:29 +0100

    UI created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
+import Switches from './components/Switches';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <Header />
+      <main className="main">
+        <Switches />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
