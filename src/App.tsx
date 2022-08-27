44d888157182933cf183cde46b3c892263b0b086
craig-dsilva <craig.s.dsilva@gmail.com>
2022-08-27 14:30:55 +0100

    frontend complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import io from 'socket.io-client';
+
 import Header from './components/Header';
 import Switches from './containers/Switches';
 
 const App = () => {
+  const socket = io();
+  
   return (
     <div className="App">
       <Header />
       <main className="main">
-        <Switches />
+        <Switches socket={socket} />
       </main>
     </div>
   );
--- src/containers/Switches.tsx
+++ src/containers/Switches.tsx
@@ -1,27 +1,47 @@
 import React, { useState } from 'react';
+import { Socket } from 'socket.io-client';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 
-const Switches = () => {
+interface SwitchInterface {
+  socket: Socket;
+}
+
+const Switches: React.FC<SwitchInterface> = ({socket}) => {
   const [s1, sets1] = useState(false);
   const [s2, sets2] = useState(false);
   const [s3, sets3] = useState(false);
   const [s4, sets4] = useState(false);
 
+  const s1Handler = () => {
+    sets1(!s1)
+    socket.emit('s1', s1)
+  }
+
+  const s2Handler = () => {
+    sets2(!s2)
+    socket.emit('s2', s2)
+  }
+
+  const s3Handler = () => {
+    sets3(!s3)
+    socket.emit('s3', s3)
+  }
+
   return (
     <div className="switches">
       <FormControlLabel
-        control={<Switch checked={s1} onChange={() => sets1(!s1)} />}
+        control={<Switch checked={s1} onChange={s1Handler} />}
         label="Switch 1"
         labelPlacement="start"
       />
       <FormControlLabel
-        control={<Switch checked={s2} onChange={() => sets2(!s2)} />}
+        control={<Switch checked={s2} onChange={s2Handler} />}
         label="Switch 2"
         labelPlacement="start"
       />
       <FormControlLabel
-        control={<Switch checked={s3} onChange={() => sets3(!s3)} />}
+        control={<Switch checked={s3} onChange={s3Handler} />}
         label="Switch 3"
         labelPlacement="start"
       />
